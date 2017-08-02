69f7edff5165aea4c28e6a17a79a7ff5b711275c
p4v4n <pavan.barcelona@gmail.com>
2017-08-02 19:59:56 +0530

    Add game-play fn with infinite loop   -add read-and-process-move helper fn with dummy-body

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -2,6 +2,12 @@
   (require [tic-tac-toe.board :as board])
   (:gen-class))
 
-(defn -main
-  [& args]
-  )
+(defn read-and-process-move [move-str]
+  (println "dummy body"))
+
+(defn game-play []
+  (while true
+    (read-and-process-move (read-line))))
+
+(defn -main []
+    (game-play))
