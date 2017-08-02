ec72956e6aec9d8ebedb25041a943d63a661e611
p4v4n <pavan.barcelona@gmail.com>
2017-08-02 20:27:49 +0530

    Add translate-move fn to generate move-index vector   -convert valid-user-input to move-index-vector

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,13 +1,23 @@
 (ns tic-tac-toe.core
-  (require [tic-tac-toe.board :as board])
+  (require [tic-tac-toe.board :as board]
+           [clojure.string :as string])
   (:gen-class))
 
+(defn translate-move [move-str]
+  (let [[row-str column-str] (seq (string/trim move-str))
+        row (->> [row-str \a]
+                 (map int)
+                 (reduce -))
+        column ((comp dec #(Integer/parseInt %) str) column-str)]
+    [row column]))
+
 (defn read-and-process-move [move-str]
-  (println "dummy body"))
+  (let [move-index (translate-move move-str)]
+  (println "dummy body")))
 
 (defn game-play []
   (while true
     (read-and-process-move (read-line))))
 
 (defn -main []
-    (game-play))
+  (game-play))
