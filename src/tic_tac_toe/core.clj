707b45fc66b4d8bbd800ddee4d24ffe5a932f661
p4v4n <pavan.barcelona@gmail.com>
2017-08-07 12:49:44 +0530

    Add option to assign user/engine to players \X and \O

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -13,22 +13,24 @@
 
 (defn engine-moves [board-state]
   (let [engine-move-pick (ai/engine-choice board-state)]
+    (Thread/sleep 1000)
     (board/update-board-state board-state engine-move-pick)))
 
-(defn next-board-state [board-state]
-  (case (:turn board-state)
-    \X (user-moves board-state)
-    \O (engine-moves board-state)))
+(def player-to-call {"human" user-moves "engine" engine-moves})
 
-(defn game-play [initial-board-state]
-  (loop [board-state initial-board-state]
+(defn next-board-state [board-state player-to-move]
+ ((player-to-call player-to-move) board-state))
+
+(defn game-play [initial-board-state player1 player2]
+  (loop [board-state initial-board-state player-to-move player1]
     (board/console-print (:board board-state))
     (if (board/game-over? (:board board-state))
         (board/end-of-game-action board-state)
-        (do (println (str "Player-" (:turn board-state) " to move:"))
-            (recur (next-board-state board-state))))))
+        (do (println (str "Player-" (:turn board-state) " to move:" "(" player-to-move ")"))
+            (recur (next-board-state board-state player-to-move)
+                   ((zipmap [player1 player2] [player2 player1]) player-to-move))))))
 
-(defn -main [board-size-str]
+(defn -main [board-size-str player1 player2]
   (let [board-length (Integer/parseInt (string/trim board-size-str))]
-    (->> (board/create-initial-board-state board-length)
-         game-play)))
+    (-> (board/create-initial-board-state board-length)
+         (game-play player1 player2))))
